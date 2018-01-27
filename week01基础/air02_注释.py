2031a72075d58a8e50443bf223b8d9f9741418b7
airvip <sdqhwzb@163.com>
2018-01-27 17:17:37 +0800

    注释

---
+++ week01基础/air02_注释.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/1/27 17:12.
+
+# 单行注释
+print("Python 注释")
+
+# 为了保证代码的可读性，一般都会在 '#' 之后天机一个空格
+
+print("# 就是 Python 单行注释符")
