a2119cce52425894e5ca82e0c6dbbaffc3d12d22
airvip <sdqhwzb@163.com>
2018-01-28 22:51:54 +0800

    变量

---
+++ week01基础/air04_变量.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/1/28 22:49.
+
+name = "airvip"
+age  = 24
+addr = "china"
+
+print("姓名：{}，年龄：{}，地址：{}".format(name,age,addr))
