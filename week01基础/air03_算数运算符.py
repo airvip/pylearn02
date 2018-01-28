198fc672685760d565f8839192e4c7b294a9d571
airvip <sdqhwzb@163.com>
2018-01-28 21:45:37 +0800

    算术运算

---
+++ week01基础/air03_算数运算符.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/1/28 21:40.
+
+# 加法
+print( 1 + 2 )
+print( "air" + "vip" )
+
+# 减法
+print( 5 - 2 )
+
+# 乘法
+print( 2 * 3 )
+print( "air" * 2 )
+
+# 除法
+print( 9 / 3 )
+
+# 求余
+print( 9 % 4 )
+
+# 除取整
+print( 9 // 4 )
+
+
