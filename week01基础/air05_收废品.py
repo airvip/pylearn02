5e9e9adb38fa8ab83f843cbe67c274735ba33883
airvip <sdqhwzb@163.com>
2018-01-29 23:56:39 +0800

    收瓶子

---
+++ week01基础/air05_收废品.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/1/29 23:53.
+
+# 有 20 斤废旧瓶子
+weight = 20
+
+# 每斤 0.75 元
+price = 0.75
+
+# 总共卖了 total 元
+total_price = price * weight
+print(total_price)
+
