92c3fa3f417a4f129e5ebdf73fd5f0b78ee9ddc3
airvip <sdqhwzb@163.com>
2018-01-31 23:54:08 +0800

    format

---
+++ week01基础/air09_格式化输出.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/1/31 23:49.
+
+print("my name is %s" % "阿尔维奇")
+
+
+print("my no is %09d" % 1)
+
+name = "airivp"
+no = 9
+print("my name is %s, my no is %09d" % (name, no))
+
+print("your no is %9d" % 10)
+
