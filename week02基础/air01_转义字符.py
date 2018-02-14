09f92b3bab3e5fe880088a15f55e4c86f292ce04
airvip <sdqhwzb@163.com>
2018-02-14 23:12:49 +0800

    转义字符

---
+++ week02基础/air01_转义字符.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by Admin on 2018/2/14 23:07.
+
+# \t  制表符：使文本在垂直方向上对其
+print("100\t200\t300")
+print("01\t02\t03")
+
+
+# \n 换行
+print("hello\nword")
