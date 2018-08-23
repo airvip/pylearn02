62e9f14fef03d8bdf0b77e5aa2de2ed60a6a47fd
airvip <sdqhwzb@163.com>
2018-08-23 17:13:54 +0800

    堆排序

--- algorithm/堆排序.py
+++ algorithm/堆排序.py
@@ -8,14 +8,19 @@ def heap_sort(nums):
     def sift_down(start, end):
         root = start
         while True:
+            # child 表示 root 节点的左孩子下标
+            # end 表示最后一个节点的下标
             child = 2 * root + 1
+            # child > end，说明 root 没有子节点
             if child > end:
                 break
+            # child + 1 < end 说明存在左孩子
+            # child + 1 = end 说明存在右孩子
             if child + 1 <= end and nums[child] < nums[child + 1]:
-                child += 1
-            if nums[root] < nums[child]:
+                child += 1  # 右孩子大就用右孩子的下标做调整
+            if nums[root] < nums[child]:  # 如果父节点小于孩子节点就交换
                 nums[root], nums[child] = nums[child], nums[root]
-                root = child
+                root = child  # 将子节点作为父节点进入下次循环判断是否有子节点
             else:
                 break
 
