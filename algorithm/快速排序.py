ea5ca1c312f81d90db06647e89e5cc29696789e1
airvip <sdqhwzb@163.com>
2018-08-23 18:12:30 +0800

    快速排序

---
+++ algorithm/快速排序.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Created by airvip on 2018/8/23 17:26.
+
+def quick_sort(nums, start, end):
+    i = start
+    j = end
+    if i < j:
+        base = nums[i]  # 设置基准，默认第一个值
+        while i < j:
+            # 如果列表后边的数比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while i < j and nums[j] >= base:
+                j -= 1
+            # 如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            nums[i] = nums[j]
+            # 如果列表前边的数比基准数小或相等,则后移一位直到有比基准数大的数出现
+            while i < j and nums[i] <= base:
+                i += 1
+            nums[j] = nums[i]
+        # 做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        nums[i] = base
+        quick_sort(nums, start, i - 1)
+        quick_sort(nums, j + 1, end)
+    return nums
+
+
+if __name__ == "__main__":
+    list_nums = [32, 24, 19, 55, 30, 22, 8]
+    list_res = quick_sort(list_nums, 0, len(list_nums) - 1)
+    print(list_res)
