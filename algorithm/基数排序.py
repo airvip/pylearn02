c85f56b99b20d43f7f23268b60d850bf30706848
airvip <sdqhwzb@163.com>
2018-09-04 18:04:46 +0800

    add 基数排序

--- algorithm/基数排序.py
+++ algorithm/基数排序.py
@@ -4,16 +4,12 @@
 
 import math
 
-def radix_sort(nums, radix=4):
-    print(math.log(max(nums), radix))
-    print(math.ceil(math.log(max(nums), radix)))
+def radix_sort(nums, radix=10):
     K = int(math.ceil(math.log(max(nums), radix)))
     bucket = [[] for i in range(radix)]
-    print(bucket)
     for i in range(1, K + 1):  # K次循环
         for val in nums:
             bucket[int(val % (radix ** i) / (radix ** (i - 1)))].append(val)  # 析取整数第K位数字 （从低到高）
-        print(bucket)
         del nums[:]
         for each in bucket:
             nums.extend(each)  # 桶合并
