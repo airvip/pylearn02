7308a5ac91bd1cd50dc1f6f64617634b39814b72
airvip <sdqhwzb@163.com>
2018-09-07 16:01:12 +0800

    shell sort

--- algorithm/希尔排序.py
+++ algorithm/希尔排序.py
@@ -2,3 +2,22 @@
 # -*- coding:utf-8 -*-
 # Created by airvip on 2018/9/5 18:14.
 
+
+
+def shell_sort(nums):
+    step = len(nums) // 2
+    while step > 0:
+        for i in range(step, len(nums)):
+            while i >= step and nums[i - step] > nums[i]:
+                nums[i], nums[i - step] = nums[i - step], nums[i]
+                i -= step
+        step = step // 2
+    return nums
+
+
+
+
+if __name__ == "__main__":
+    list_nums = [32, 24, 19, 55, 30, 22, 8]
+    list_res = shell_sort(list_nums)
+    print(list_res)
